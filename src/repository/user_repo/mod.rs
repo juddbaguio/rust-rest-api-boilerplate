ce24ed83b5b0f023425da4c87a01db71cc0809e7
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2023-10-03 08:06:35 +0800

    changes

--- src/repository/user_repo/mod.rs
+++ src/repository/user_repo/mod.rs
@@ -11,44 +11,6 @@ use crate::domain::{
 
 use super::models::users;
 
-// #[allow(dead_code)]
-// pub async fn create_user<'a, E>(
-//     executor: E,
-//     payload: CreateUserDTO,
-// ) -> Result<entities::User, Box<dyn Error + Send + Sync>>
-// where
-//     E: 'a + Executor<'a, Database = Postgres>,
-// {
-//     let create_user_stmt = sqlx::query_as::<_, users::Model>(
-//         r#"
-//             INSERT INTO users (
-//                 first_name,
-//                 middle_name,
-//                 last_name,
-//                 username,
-//                 password
-//             )
-//             values(
-//                 $1,
-//                 $2,
-//                 $3,
-//                 $4,
-//                 $5
-//             )
-//             RETURNING *
-//         "#,
-//     )
-//     .bind(payload.first_name)
-//     .bind(payload.middle_name)
-//     .bind(payload.last_name)
-//     .bind(payload.username)
-//     .bind(payload.password);
-
-//     let create_user_res = create_user_stmt.fetch_one(executor).await?;
-
-//     Ok(entities::User::from(create_user_res))
-// }
-
 pub struct UserRepositoryInteractor<'a> {
     #[allow(dead_code)]
     pub conn: &'a mut PgConnection,
